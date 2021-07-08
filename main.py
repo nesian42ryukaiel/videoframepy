b61ea82d949194a55abe15122560c0e7f5e474ed
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-07-08 18:16:59 +0900

    part 1 practically finished

--- main.py
+++ main.py
@@ -10,6 +10,9 @@
 import cv2
 
 vidcap = cv2.VideoCapture('./video/test.mp4')
+fps = vidcap.get(cv2.CAP_PROP_FPS)
+totalNoFrames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+durationInSeconds = float(totalNoFrames) / float(fps)
 
 count = 0
 
@@ -17,8 +20,8 @@ while(vidcap.isOpened()):
     ret, image = vidcap.read()
     image = cv2.resize(image, (960, 540))
 
-    if (int(vidcap.get(1)) % 30 == 0):
+    if (int(vidcap.get(1)) % int(fps) == 0):
         print('Saved frame number : ' + str(int(vidcap.get(1))))
         cv2.imwrite('./stash/frame%d.png' % count, image)
         count += 1
-        vidcap.release()
+        if count >= durationInSeconds: vidcap.release()
