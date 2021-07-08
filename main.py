ebbfc24f03fd197a0d029f2d3d72c65afc89572b
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-07-08 19:31:14 +0900

    hierarchy modified

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 # assumed to be starting at home directory
 # activate venv by:
 #     % source ~/venv/firstvenv/bin/activate
-# link at:
-#     % python3 ~/repo/videoframepy/main.py
+# change directory by:
+#     % cd ~/repo/videoframepy
+# activate by:
+#     % python3 main.py
 # -------------------------------------------------------------------- #
 
 import cv2
 
-vidcap = cv2.VideoCapture('./video/test.mp4')
+vidcap = cv2.VideoCapture('./test/input/test.mp4')
 fps = vidcap.get(cv2.CAP_PROP_FPS)
 totalNoFrames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
 durationInSeconds = float(totalNoFrames) / float(fps)
@@ -22,6 +24,6 @@ while(vidcap.isOpened()):
 
     if (int(vidcap.get(1)) % int(fps) == 0):
         print('Saved frame number : ' + str(int(vidcap.get(1))))
-        cv2.imwrite('./stash/frame%d.png' % count, image)
+        cv2.imwrite('./test/output/frame%d.png' % count, image)
         count += 1
         if count >= durationInSeconds: vidcap.release()
