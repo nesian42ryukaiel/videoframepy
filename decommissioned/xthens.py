4f498db482dce063535e6e39a0d010c83833152f
Lucas Yew <54085954+nesian42ryukaiel@users.noreply.github.com>
2021-07-26 20:12:48 +0900

    Delete decommissioned directory

--- decommissioned/xthens.py
+++
@@ -1,9 +0,0 @@
-from videoframepy import extractor as xtr
-from videoframepy import requester as snd
-
-
-def xthens():
-    address = 'http://localhost:5000'
-    test_url = address + '/api/test'
-    x = xtr.Extractor('test.jpg', 30)
-    s = snd.Requester(test_url)
